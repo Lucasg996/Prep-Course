d7e01a5e5d21b5569784f455a6d27040fe023c61
Lucasg996 <gonzalezlucas96@gmail.com>
2022-03-01 14:46:05 -0300

    a

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,20 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+
+  var=crearGato {
+
+    nombre: nombre,
+    edad: edad,
+
+    function meow(){
+
+    return "Meow!";
+    }
+    return crearGato;
+  }
+
+
 }
 
 
